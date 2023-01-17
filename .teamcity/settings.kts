999cdeaf830f28b171f2037622049e6817318084
nodoby nobodin <n.chubatova@gmail.com>
2023-01-17 14:58:16 +0100

    changeid

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -6,7 +6,7 @@ version = "2022.10"
 
 project {
 
-    buildType(Build210xyz0nd)
+    buildType(Build210xyz0ndp)
     buildType(Build1130xyznc)
 }
 
@@ -32,7 +32,7 @@ object Build1130xyznc : BuildType({
     }
 })
 
-object Build210xyz0nd : BuildType({
+object Build210xyz0ndp : BuildType({
     name = "build2"
 
     artifactRules = "+:*.txt => ."
