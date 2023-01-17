cec813fece88526f83b8ab98b7976a6cbaa9054f
admin <admin@TeamCity>
2023-01-17 07:38:32 -0800

    TeamCity change in '74019 / project1 / subpr1' project: 1 build step(s) were added to 'gradle' build configuration

--- .teamcity/patches/buildTypes/Subpr1_Gradle.kts
+++ .teamcity/patches/buildTypes/Subpr1_Gradle.kts
@@ -2,6 +2,7 @@ package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.BuildType
+import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.ui.*
 
 /*
@@ -15,6 +16,13 @@ create(RelativeId("Subpr1"), BuildType({
 
     vcs {
         root(RelativeId("Subpr1_HttpsGithubComChubatovaTigerChubatovaGradleTestsBackup"))
+    }
+
+    steps {
+        gradle {
+            tasks = "clean build"
+            gradleWrapperPath = ""
+        }
     }
 }))
 
