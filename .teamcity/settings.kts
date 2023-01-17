c06aa0dc2cd67f6ff93dc926af77fc335cac3bad
admin <admin@TeamCity>
2023-01-17 07:49:46 -0800

    TeamCity change in '74019 / project1 / subpr1' project: snapshot dependency was removed in 'b11' build configuration

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -176,11 +176,6 @@ object Subpr1_B1111 : BuildType({
             scriptContent = "echo hi"
         }
     }
-
-    dependencies {
-        snapshot(AbsoluteId("id74019_Project2_Subpr1_B11")) {
-        }
-    }
 })
 
 object Subpr1_B121 : BuildType({
