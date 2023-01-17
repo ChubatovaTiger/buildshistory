0d4193a869fbeb8025a6706ef25b8f4a825de420
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-01-17 14:21:26 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -27,7 +27,7 @@ version = "2022.10"
 
 project {
 
-    buildType(Build2)
+    buildType(Build210)
     buildType(Build1130)
 }
 
@@ -53,7 +53,7 @@ object Build1130 : BuildType({
     }
 })
 
-object Build2 : BuildType({
+object Build210 : BuildType({
     name = "build2"
 
     artifactRules = "+:*.txt => ."
